d011c129f2b9bd0f767260af1af97bb7dfacd758
Ondrej Doktor <doktor.ml@gmail.com>
2019-10-27 12:29:44 +0100

    WPF basics init (import z cviceni na hodine) - Ondra

---
+++ WPF_basics/WPF_basics/ViewModels/Convertors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+/// <summary>
+/// This is only separate .cs file, there is no real Convertors class.
+/// </summary>
+namespace WPF_basics.ViewModels
+{
+    public class BooleanToYesNoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null) // Always check value (and parameter if used)
+            {
+                bool val = (bool)value;
+                if (val) return "Yes";
+                else return "No";
+            }
+            return "IDK"; // Fallback
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ WPF_basics/WPF_basics/Windows/ElementBinding.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_basics.Windows
+{
+    /// <summary>
+    /// Interaction logic for ElementBinding.xaml
+    /// </summary>
+    public partial class ElementBinding : Window
+    {
+        public ElementBinding()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Close_Command(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Close_Command_CanEx(object sender, CanExecuteRoutedEventArgs e)
+        {
+            // Always active.
+            e.CanExecute = true;
+        }
+    }
+}
