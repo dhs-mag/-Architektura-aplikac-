693179c642d0eabcf4043f96c3e3f2a33bcc3478
Ondrej Doktor <doktor.ml@gmail.com>
2019-10-27 12:35:56 +0100

    INIT verze ze ZIP z Elearning

---
+++ KingsQuest/Engine/KQEngine.cs
@@ -0,0 +1,43 @@
+﻿using Data;
+using System;
+
+namespace Engine
+{
+    public class KQEngine
+    {
+        private Room currentRoom;
+
+        public void PrintState()
+        {
+            //Console.WriteLine("You are in a Forest! You know nothing about the Forest John!");
+            Console.WriteLine(currentRoom.Description);
+        }
+
+        public void GameInit()
+        {
+            Room forest = new Room() { Name = "Forest", Description = "You are in a Forest! You know nothing about the Forest John!" };
+            Room chamber = new Room() { Name = "Chamber", Description = "You are in a black chamber!" };
+            chamber.NRooms.Add(forest);
+            forest.NRooms.Add(chamber);
+            currentRoom = forest;
+        }
+
+        public void Update()
+        {
+            bool quit = false;
+            while (!quit)
+            {
+                //Console.Clear();
+                PrintState();
+                string input = Console.ReadLine();
+                switch(input.ToLower())
+                {
+                    case "exit": quit = true;
+                        break;
+                    default: Console.WriteLine($"You wrote: {input}");
+                        break;
+                }
+            }
+        }
+    }
+}
