1b4d7b15f5e9ccecc570f5b53e65b8d06cf7ff09
Jirka H <jirihauser94@gmail.com>
2019-11-05 17:32:07 +0100

    command parsing

---
+++ Engine/KQEngine.cs
@@ -0,0 +1,92 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Engine
+{
+    public class KQEngine
+    {
+        private Room currentRoom;
+        public Room CurrentRoom { get => currentRoom; }
+        public Player Hero { get; set; }
+        
+        public void GameInit()
+        {
+            
+            //items
+            Item sword = new Item() { Name = "Excalibur", Description = "Shiny, silver, deadly", type = ItemType.WEAPON};
+            Item needle = new Item() { Name = "Needle", Description = "Thin, but can do hella circus", type = ItemType.WEAPON};
+            Item money = new Item() { Name = "Money", Description = "Small but cute bag of gold", type = ItemType.MERCH};
+            
+            // Player - you 
+            Player player = new Player();
+            player.Items.Add(needle);
+            player.Items.Add(money);
+            Hero = player;    
+            
+            // persons
+            Person Arthur = new Person() {Name = "Arthur", Story = "I am here to save the princess", nature = PersonType.FRIEND};
+            Arthur.Items.Add(sword);
+            Arthur.Items.Add(money);
+            // rooms
+            Room forest = new Room() { Name = "Forest", Description = "You are in a Forest! You know nothing John Forest!" };
+            Room chamber = new Room() { Name = "Chamber", Description = "You are in a black chamber!" };
+            chamber.NRooms.Add(forest);
+            forest.NRooms.Add(chamber);
+            forest.Persons.Add(Arthur);
+            
+            currentRoom = forest;
+        }
+        
+        public void Command_GO(Room newRoom)
+        {
+            if (this.currentRoom.NRooms.Contains(newRoom)) currentRoom = newRoom;
+        }
+
+        public string Command_TALK(Person person)
+        {
+            return person.Story;
+        }
+        
+        public string Command_USE(Item item, Person person)
+        {
+            if (item.type == ItemType.WEAPON)
+            {
+                switch (person.nature)
+                {
+                    case PersonType.FOE:
+                        person.Stamina -= 10;
+                        return $"You hit {person.Name} with {item.Name} and person's stamina is now equal to {person.Stamina}";
+                    case PersonType.FRIEND:
+                        return $"{person.Name} is not hostile, do not attack him!";
+                    default:
+                        return "";
+                }
+            }
+            
+            if (item.type == ItemType.POTION)
+            {
+                switch (person.nature)
+                {
+                    case PersonType.FOE:
+                        return "Potion has no effect";
+                    case PersonType.FRIEND:
+                        person.Stamina += 10;
+                        return $"{person.Name} is affected by {item.Name} and person's stamina is now equal to {person.Stamina}!";
+                    default:
+                        return "";
+                }
+            }
+
+            return "";
+        }
+
+        
+        public void Command_Exit()
+        {
+            //do something smart (save progres, etc.)
+        }
+
+    }
+}
---
+++ Game/Program.cs
@@ -0,0 +1,109 @@
+﻿using Data;
+using Engine;
+using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Game
+{
+    class Program
+    {
+        public static KQEngine Engine { get; set; }
+        
+
+        static void Main(string[] args)
+        {
+            Engine = new KQEngine();
+            Engine.GameInit();
+
+            bool quit = false;
+            while (!quit)
+            {
+                // gameInfo
+                Console.WriteLine(Engine.CurrentRoom.Description);
+                string RoomPersons = getRoomPersons();
+                Console.WriteLine(RoomPersons != null ? $"Persons in the room: {RoomPersons}" : "You are alone in the room...");
+                Console.WriteLine($"Possible routes: {getPossibleRoutes()}");
+                Console.WriteLine($"Your items: {getItems()}");
+                Console.WriteLine();
+                Console.WriteLine(getGameInterface());
+                
+                string input = Console.ReadLine();
+                
+                Console.WriteLine("==========================");
+                parseCommand(input);
+                Console.WriteLine("==========================");
+            }
+        }
+
+        public static void parseCommand(string input)
+        {
+            string[] buffer = input.Split(' ');
+            string command = buffer[0].ToLower();
+            if (command == "go")
+            {
+                if (buffer.Length > 1)
+                {
+                    Room newRoom = Engine.CurrentRoom.NRooms.First(x => x.Name.ToLower() == buffer[1].ToLower());
+                    Engine.Command_GO(newRoom);    
+                }
+            }
+            if (command == "talk")
+            {
+                if (buffer.Length > 1)
+                {
+                    Person person = Engine.CurrentRoom.Persons.First(p => p.Name.ToLower() == buffer[1].ToLower());
+                    Console.WriteLine(Engine.Command_TALK(person));
+                }
+            }
+            if (command == "use")
+            {
+                if (buffer.Length > 2)
+                {
+                    Item item = Engine.Hero.Items.First(i => i.Name.ToLower() == buffer[1].ToLower());
+                    Person person = Engine.CurrentRoom.Persons.First(p => p.Name.ToLower() == buffer[2].ToLower());
+                    Console.WriteLine(Engine.Command_USE(item, person));
+                }
+            }
+            Console.WriteLine();
+        }
+        
+        public static string getPossibleRoutes()
+        {
+            string routes = "";
+            foreach (Room room in Engine.CurrentRoom.NRooms)
+            {
+                routes += room.Name;
+            }
+
+            return routes.Length > 0 ? routes : null;
+        }
+
+        public static string getRoomPersons()
+        {
+            string persons = "";
+            foreach (Person person in Engine.CurrentRoom.Persons)
+            {
+                persons += person.Name + " ";
+            }
+
+            return persons.Length > 0 ? persons : null;
+        }
+
+        public static string getItems()
+        {
+            string output = "";
+            foreach (Item item in Engine.Hero.Items)
+            {
+                output += item.Name + " ";
+            }
+
+            return output;
+        }
+        public static string getGameInterface()
+        {
+            string output = "Possible commands are:\n go <Room>\n talk <Person>\n use <Item> <Person>";
+            return output;
+        }
+    }
+}
